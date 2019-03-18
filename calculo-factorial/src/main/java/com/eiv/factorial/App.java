331dc426131a964ba81fce6fd6704c777b553cf8
pablo.raspo <pablo.raspo@EQPAR.eivsoftware.net>
2019-03-18 16:30:44 -0300

    Se agrego la interface y la clase factorial.

--- calculo-factorial/src/main/java/com/eiv/factorial/App.java
+++ calculo-factorial/src/main/java/com/eiv/factorial/App.java
@@ -1,10 +1,19 @@
 package com.eiv.factorial;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Factorial!");
 
+		CalculoFactorial mCalculoFactorial = new CalculoFactorialImpl();
+		
+		System.out.println("Cargale un número!:");
+		Scanner sStr = new Scanner(System.in);
+		String sNumero = sStr.nextLine();
+		int el_numero = Integer.valueOf(sNumero);
+		long resultado = mCalculoFactorial.calcular(el_numero);
+		sStr.close();
+		System.out.println("El resultado del factorial de " + el_numero + " es " + resultado);
 	}
-
 }
---
+++ calculo-factorial/src/main/java/com/eiv/factorial/CalculoFactorialImpl.java
@@ -0,0 +1,21 @@
+package com.eiv.factorial;
+
+public class CalculoFactorialImpl implements CalculoFactorial {
+
+	@Override
+	public long calcular(int numero) {
+		long resultado = 1;
+
+		for(int indice = 1; indice < numero; indice++) {
+			resultado = resultado * (indice + 1);
+		};
+		
+		return resultado;
+	}
+	
+	public String detalle() {
+		return "Calculo algo!";
+	} 
+	
+	
+}
